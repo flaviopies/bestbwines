d036f7a61b34a67c027b52f68f22c6be2a231611
Flavio <flaviopies@poli.ufrj.br>
2017-08-19 12:20:20 -0300

    Creation of pagination loop returning a pandas dataframe:
    Improvements in the crawler functions
    - Wine name
    - Wine description
    - Wine price

--- README.md
+++ README.md
@@ -1,7 +1,7 @@
 # Bestbwines
 
 BestbWine aims to finds good buys into the wine market. It does so crawling two websites:
-* wine.com.br
+* www.wine.com.br
     * Crawls the offers and gathers wine infos, such as name and price
 * www.vivino.com
     * Brings reviews from users from the wine social media
--- winecrawler.py
+++ winecrawler.py
@@ -1,24 +1,26 @@
 import requests
+import re
+import pandas as pd
 from bs4 import BeautifulSoup
 
-site_tinto = 'https://www.wine.com.br/vinhos/tinto/cVINHOS-atTIPO_TINTO-p1.html'
-site_branco = "https://www.wine.com.br/vinhos/branco/cVINHOS-atTIPO_BRANCO-p1.html"
-site_rose = "https://www.wine.com.br/vinhos/rose/cVINHOS-atTIPO_ROSE-p1.html"
-site_espumante = "https://www.wine.com.br/vinhos/espumante/cVINHOS-atTIPO_ESPUMANTE-p1.html"
-site_licoroso = "https://www.wine.com.br/vinhos/licoroso/cVINHOS-atTIPO_LICOROSO-p1.html"
+#site = "https://www.wine.com.br/browse.ep?cID=100851&filters=&pn=1&exibirEsgotados=false&listagem=horizontal&sorter=featuredProducts-desc"
+site = "https://www.wine.com.br/vinhos/tinto/cVINHOS-atTIPO_TINTO-p1.html"
 
-site = site_tinto
-def wine_request(site):
-    r = requests.get(site)
-    if r.status_code = 200:
-        output = r.text
-    else
+def wine_request(site,page):
+    r = requests.get(site,{"pn": page})
+    if r.status_code == 200:
+        output = r
+        print("Request successful")
+    else:
         output = r.status_code
+        print("Request failed")
     return output
+
 
 def wine_soup(r):
     soup = BeautifulSoup(r.text,"html.parser")
     return soup
+
 
 def find_wine_names(soup):
     wine_names_list = []
@@ -27,17 +29,21 @@ def find_wine_names(soup):
         wine_names_list.append(name.h2.a.string)
     return wine_names_list
 
+
 def find_wine_description(soup):
-    wine_description_list = [[],[],[]]
+    wine_description_list = [[],[],[],[],[]]
     description_list = soup.select(".descProdutos")
     for description in description_list:
         wine_description_list[0].append(description.ul.li.strong.string)
-        wine_description_list[1].append(description.ul.li.span.string.split(", "))
+        wine_description_list[1].append(description.ul.li.span.string.split(", ")[0])
+        wine_description_list[2].append(description.ul.li.span.string.split(", ")[1])
         wine_type_vol = description.ul.li.next_sibling.next_sibling.strong.string
         wine_type_vol = re.sub('[\t\n+]', '', wine_type_vol)
         wine_type_vol = wine_type_vol.split("- ")
-        wine_description_list[2].append(wine_type_vol)
+        wine_description_list[3].append(wine_type_vol[0])
+        wine_description_list[4].append(wine_type_vol[1])
     return wine_description_list
+
 
 def find_wine_prices(soup):
     wine_price_list = []
@@ -46,3 +52,24 @@ def find_wine_prices(soup):
         price_value = price.p.strong.span.next_sibling.string
         wine_price_list.append(price_value.strip(" "))
     return wine_price_list
+
+
+def pagination_dataframe(site, n_max):
+    df = pd.DataFrame()
+    pages_iteration = range(1, n_max)
+
+    for page in pages_iteration:
+        temp_df = pd.DataFrame()
+        r = wine_request(site, page)
+        soup = wine_soup(r)
+        temp_df["Names"] = find_wine_names(soup)  # names
+        temp_df["Winery"] = find_wine_description(soup)[0]  # winery
+        temp_df["Region"] = find_wine_description(soup)[1]  # region
+        temp_df["Country"] = find_wine_description(soup)[2]  # country
+        temp_df["Type"] = find_wine_description(soup)[3]  # type
+        temp_df["Volume"] = find_wine_description(soup)[4]  # volume
+        temp_df["Price"] = find_wine_prices(soup)  # price
+        df = pd.concat([df,temp_df],axis=0)
+        del temp_df
+
+    return df
